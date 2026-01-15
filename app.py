0e87b850fd87d1a02bfb50500f2e5abd54cfa712
Pranav Choudhary <pc380@snu.edu.in>
2026-01-15 22:14:47 +0530

    DAY - 2
    IMPORT OF Py.PDF2 AND EXTRACT THE PDF LINES PAGES ...

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+
 
 st.set_page_config(page_title="AI Resume Analyzer", layout="centered")
 
 st.title("AI Resume Analyzer & Job Matcher")
 st.write("Upload your resume and get AI-powered insights.")
+st.write("Built by Pranav")
+
 
 uploaded_file = st.file_uploader("Upload your Resume (PDF)", type=["pdf"])
 
 if uploaded_file is not None:
+    reader = PdfReader(uploaded_file)
+    resume_text = ""
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            resume_text += text + "\n"
+    
     st.success("Resume uploaded successfully!")
+
+    st.subheader("Extracted Resume Text:")
+    st.text(resume_text)
 
